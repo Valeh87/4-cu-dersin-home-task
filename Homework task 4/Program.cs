86f0a3e0c6b08be641f2e323accc9808570233bd
Valeh Aghayev <110283642+Valeh87@users.noreply.github.com>
2022-08-10 23:05:06 +0400

    first commit

---
+++ Homework task 4/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Homework_task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 task
+            //  DateTime mydata = new DateTime(1987, 07, 24);
+            //Console.WriteLine(mydata.DayOfWeek);
+            #endregion
+
+            #region tapsiriq 2
+            //DateTime dateTime = new DateTime(1987, 07, 24);
+            //DateTime Avqust = new DateTime(2022, 08, 10);
+            //TimeSpan zaman = Avqust.Subtract(dateTime);
+            //Console.WriteLine(zaman.ToString());
+            #endregion
+
+            #region 3 tapsiriq
+
+            //Console.WriteLine("en boyuk olan reqemi yazin");
+            //int a, b,c;
+            //Console.Write("1ci reqemi qeyd edin");
+            //a=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("2ci reqemi qeyd edin");
+            //b=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("3cu reqemi qeyd edin");
+            //c=Convert.ToInt32(Console.ReadLine());
+            //if (a > b)
+
+            //{
+            //    Console.WriteLine("1 ci reqem boyukdur");
+            //    if (a < b)
+            //    {
+            //        Console.WriteLine("2 ci reqem boyukdur");
+            //    }
+
+            //}
+            //else if (a < c )
+
+            //{
+            //    Console.WriteLine("3 cu reqem boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("2 ci reqem boyukdur");
+            //}
+            #endregion
+
+            #region 4 cu tapsiriq
+
+            //int hesablama;
+            //hesablama = Convert.ToInt32(Console.ReadLine());
+            //switch (hesablama)
+            //{
+            //    case 1:
+            //        Console.WriteLine("bazar ertesi");
+            //        break;
+            //        case 2:
+            //        Console.WriteLine("cersenbe axsami");
+            //        break ;
+            //        case 3:
+            //        Console.WriteLine("cersenbe");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("cume axsami");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("cume");
+            //        break;
+
+            //    case 6:
+            //        Console.WriteLine("senbe");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("bazar");
+            //        break;
+
+            //    default: 
+            //        Console.WriteLine("yanlisdir");
+            //        break;
+            #endregion
+
+        }
+    }
+}
